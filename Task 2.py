98d223bf88c8d3068ebae80f9f7b623df18b6cd9
PiezoDelRey <piezodelrey@users.noreply.github.com>
2019-01-23 12:17:59 +0800

    Hard code in Task 1

--- Task 2.py
+++ Task 2.py
@@ -1,11 +1,12 @@
 # The app intends to display interactive auction board information including
 # Item number, number of bids, description and reserve price.
-ItemName = ["Phone", "Postcard", "Car", "Pen", "Notebook", "Pencil Case", "Laptop", "Mouse", "Keyboard", ""]
+
+ItemName = ["Phone", "Postcard", "Car", "Pen", "Tablet", "Pencil Case", "Laptop", "Mouse", "Keyboard", "收藏加购物车十元返现"]
 NumberOfBid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-Description = ["", "", "", "", "", "", "", "", "", ""]
-ReservePrice = []
+Description = ["iPhone XS", "New Zealand City View", "BMW i8", "Schneider Limited Edition", "iPad Pro 12.9in", "Kokuyo Limited", "ASUS Zenbook Pro 15", "Magic Mouse", "Cherry MX. 8.0", "勿拍此项"]
+ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699]
 ProductSelling = 0
-ItemNo = []
+ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # while True:
 #         ProductSelling = int(input("Number of selling items: "))
@@ -22,17 +23,22 @@ ItemNo = []
 #     else:
 #         print("Item number is not unique, please try again.")
 #         continue
-CurrentDescription = str(input("Please enter the item description: "))
-Description.append(CurrentDescription)
-CurrentReservePrice = float(input("Please enter the price: "))
-ReservePrice.append(CurrentReservePrice)
-NumberOfBid.append(0)
+#         CurrentDescription = str(input("Please enter the item description: "))
+#         Description.append(CurrentDescription)
+#         CurrentReservePrice = float(input("Please enter the price: "))
+#         ReservePrice.append(CurrentReservePrice)
+#         NumberOfBid.append(0)
 print("\n")
+print(ItemNo)
+print(ItemName)
+print(Description)
+print(ReservePrice)
+
 
 # Task 2
 while True:
-    SearchDescription = input("Please enter the item number: ")
-    if SearchDescription not in ItemNo:
+    SearchDescription = input("Please enter the item name: ")
+    if SearchDescription not in ItemName:
         print("Item number invalid, enter again.")
         continue
     else:
