e631d9ad1e4e08cc130735b88b50b889c5a10f40
PiezoDelRey <piezodelrey@users.noreply.github.com>
2019-01-23 11:39:01 +0800

    Hard code in Task 1

--- Task 1.py
+++ Task 1.py
@@ -7,27 +7,27 @@ ReservePrice = []
 ProductSelling = 0
 ItemNo = []
 
-while True:
-        ProductSelling = int(input("Number of selling items: "))
-        if ProductSelling < 10:
-            continue
-            print("Please enter an item number bigger than 10.")
-        else:
-            break
-
-for i in range(ProductSelling):
-    CurrentItemNo = int(input("Assign an item number: "))
-    if CurrentItemNo not in ItemNo:
-        ItemNo.append(CurrentItemNo)
-    else:
-        print("Item number is not unique, please try again.")
-        continue
-    CurrentDescription = str(input("Please enter the item description: "))
-    Description.append(CurrentDescription)
-    CurrentReservePrice = float(input("Please enter the price: "))
-    ReservePrice.append(CurrentReservePrice)
-    NumberOfBid.append(0)
-    print("\n")
+# while True:
+#         ProductSelling = int(input("Number of selling items: "))
+#         if ProductSelling < 10:
+#             continue
+#             print("Please enter an item number bigger than 10.")
+#         else:
+#             break
+#
+# for i in range(ProductSelling):
+#     CurrentItemNo = int(input("Assign an item number: "))
+#     if CurrentItemNo not in ItemNo:
+#         ItemNo.append(CurrentItemNo)
+#     else:
+#         print("Item number is not unique, please try again.")
+#         continue
+CurrentDescription = str(input("Please enter the item description: "))
+Description.append(CurrentDescription)
+CurrentReservePrice = float(input("Please enter the price: "))
+ReservePrice.append(CurrentReservePrice)
+NumberOfBid.append(0)
+print("\n")
 print(ItemNo)
 print(Description)
 print(ReservePrice)
