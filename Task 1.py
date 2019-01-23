9378f66b6d01287a9dfe4f284681510702263877
PiezoDelRey <piezodelrey@users.noreply.github.com>
2019-01-23 20:41:03 +0800

    Delete part that do not necessarily needed in the exam

--- Task 1.py
+++ Task 1.py
@@ -4,9 +4,9 @@
 ItemName = ["Phone", "Postcard", "Car", "Pen", "Tablet", "Pencil Case", "Laptop", "Mouse", "Keyboard", "收藏加购物车十元返现"]
 NumberOfBid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 Description = ["iPhone XS", "New Zealand City View", "BMW i8", "Schneider Limited Edition", "iPad Pro 12.9in", "Kokuyo Limited", "ASUS Zenbook Pro 15", "Magic Mouse", "Cherry MX. 8.0", "勿拍此项"]
-ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699]
+ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699, 1000000000]
 ProductSelling = 0
-ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # while True:
 #         ProductSelling = int(input("Number of selling items: "))
--- Task 2.py
+++ Task 2.py
@@ -1,45 +1,32 @@
 # The app intends to display interactive auction board information including
 # Item number, number of bids, description and reserve price.
 
-ItemName = ["Phone", "Postcard", "Car", "Pen", "Tablet", "Pencil Case", "Laptop", "Mouse", "Keyboard", "收藏加购物车十元返现"]
+# Task 1
+
+ItemName = ["phone", "postcard", "car", "pen", "tablet", "pencil case", "laptop", "mouse", "keyboard", "收藏加购物车十元返现"]
 NumberOfBid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 Description = ["iPhone XS", "New Zealand City View", "BMW i8", "Schneider Limited Edition", "iPad Pro 12.9in", "Kokuyo Limited", "ASUS Zenbook Pro 15", "Magic Mouse", "Cherry MX. 8.0", "勿拍此项"]
-ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699]
+ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699, 1000000000]
 ProductSelling = 0
-ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# while True:
-#         ProductSelling = int(input("Number of selling items: "))
-#         if ProductSelling < 10:
-#             continue
-#             print("Please enter an item number bigger than 10.")
-#         else:
-#             break
-#
-# for i in range(ProductSelling):
-#     CurrentItemNo = int(input("Assign an item number: "))
-#     if CurrentItemNo not in ItemNo:
-#         ItemNo.append(CurrentItemNo)
-#     else:
-#         print("Item number is not unique, please try again.")
-#         continue
-#         CurrentDescription = str(input("Please enter the item description: "))
-#         Description.append(CurrentDescription)
-#         CurrentReservePrice = float(input("Please enter the price: "))
-#         ReservePrice.append(CurrentReservePrice)
-#         NumberOfBid.append(0)
-print("\n")
-print(ItemNo)
-print(ItemName)
-print(Description)
-print(ReservePrice)
 
 
 # Task 2
+
+for i in range(len(ItemName)):
+    ItemNoCurrent = str(ItemNo[i])
+    ItemNameCurrent = str(ItemName[i])
+    print(ItemNoCurrent+": "+ItemNameCurrent)
+
 while True:
     SearchDescription = input("Please enter the item name: ")
+    SearchDescription = SearchDescription.casefold()
     if SearchDescription not in ItemName:
         print("Item number invalid, enter again.")
         continue
     else:
-        CurrentPosition = ItemNo.index()
+        CurrentPosition = ItemName.index(SearchDescription)
+        CurrentDescription = Description[CurrentPosition]
+        print("Details: "+CurrentDescription)
+        print("Current highest bit is "+)
