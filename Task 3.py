167c5c41f666278cd05e3ccab7d87a224e589394
PiezoDelRey <piezodelrey@users.noreply.github.com>
2019-01-24 18:33:42 +0800

    Change buyer id to buyer number, allow the whole loop to break.

--- Task 3.py
+++ Task 3.py
@@ -12,16 +12,21 @@ item_number_list = list(range(1, 11))
 # Task 2
 
 highest_bit_list = [0]*10
-buyer_id_list = ["Steven", "John", "Charles", "Sam", "Mr. Matthews", "Mr. Rigby"]
+buyer_number_list = ["1", "2", "3", "4", "5", "6"]
 item_highest_bid_holder_list = [""]*10  # used in task 3
 
-for i in range(len(item_name_list)):
-    item_number_current = str(item_number_list[i])
-    current_item_name = str(item_name_list[i])
-    print(item_number_current + ": " + current_item_name)
+# for i in range(len(item_name_list)):
+#     item_number_current = str(item_number_list[i])
+#     current_item_name = str(item_name_list[i])
+#     print(item_number_current + ": " + current_item_name)
 
 while True:
+    for i in range(len(item_name_list)):
+        item_number_current = str(item_number_list[i])
+        current_item_name = str(item_name_list[i])
+        print(item_number_current + ": " + current_item_name)
     restart = False
+    exit_loop = False
     name_search = input("Please enter the item name: ")
     name_search = name_search.casefold()
     if name_search not in item_name_list:
@@ -42,7 +47,7 @@ while True:
 
         while purchase_status == "y":
             buyer_id_check = str(input("Please enter your buyer ID: "))
-            while buyer_id_check in buyer_id_list:
+            while buyer_id_check in buyer_number_list:
                 print("\nIdentity verified.")
                 buyer_bid = float(input("Please enter your bid: "))
                 # no type check or type conversion is need because if it is not a number,
@@ -53,26 +58,40 @@ while True:
                     bid_number_list[search_index] += 1
                     item_highest_bid_holder_list[search_index] = buyer_id_check
                     print("Congratulation! Your bid is the current highest.")
-                    print("Yet you are free to give another higher bid.")
-                    further_bid = input("Do you want to give another bid? Y/N")
-                    further_bid = further_bid.casefold()
-                    if further_bid == "n":
-                        restart = True
-                        break
+                    print("\nYet you are free to give another higher bid.")
+                    while True:
+                        further_bid = input("Do you want to give another bid or allow others to bid? Y/N")
+                        further_bid = further_bid.casefold()
+                        if further_bid == "y":
+                            restart = True
+                            break
+                        elif further_bid == "n":
+                            exit_loop = True
+                            break
+                        else:
+                            print("Sorry, error in input")
+                            continue
                 else:
                     print("Your bid is lower than the current highest bid, please try again.\n")
                 if restart:
+                    break
+                elif exit_loop:
                     break
             else:
                 print("Identify verification failed. Please try again.")
                 continue
             if restart:
                 break
+            elif exit_loop:
+                break
         else:
             print("Purchasing process canceled.\n")
             continue
         if restart:
+            print()
             continue
+        elif:
+            break
 
 # Task 3
 
