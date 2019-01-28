8d46a2668df6978731c60a677ba729d68fbcb017
PiezoDelRey <piezodelrey@users.noreply.github.com>
2019-01-28 11:50:38 +0800

    Better Task 1, so it allows user to input selling item details.

--- Task 1.py
+++ Task 1.py
@@ -1,35 +1,38 @@
 # The app intends to display interactive auction board information including
 # Item number, number of bids, description and reserve price.
 
-ItemName = ["phone", "postcard", "car", "pen", "tablet", "pencil case", "laptop", "mouse", "keyboard", "收藏加购物车十元返现"]
-NumberOfBid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-Description = ["iPhone XS", "New Zealand City View", "BMW i8", "Schneider Limited Edition", "iPad Pro 12.9in", "Kokuyo Limited", "ASUS Zenbook Pro 15", "Magic Mouse", "Cherry MX. 8.0", "勿拍此项"]
-ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699, 1000000000]
-# ProductSelling = 0
-ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# ItemName = ["phone", "postcard", "car", "pen", "tablet", "pencil case", "laptop", "mouse", "keyboard", "收藏加购物车十元返现"]
+# NumberOfBid = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# Description = ["iPhone XS", "New Zealand City View", "BMW i8", "Schneider Limited Edition",
+# "iPad Pro 12.9in", "Kokuyo Limited", "ASUS Zenbook Pro 15", "Magic Mouse", "Cherry MX. 8.0", "勿拍此项"]
+# ReservePrice = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699, 1000000000]
+# # ProductSelling = 0
+# ItemNo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-# while True:
-#         ProductSelling = int(input("Number of selling items: "))
-#         if ProductSelling < 10:
-#             continue
-#             print("Please enter an item number bigger than 10.")
-#         else:
-#             break
-#
-# for i in range(ProductSelling):
-#     CurrentItemNo = int(input("Assign an item number: "))
-#     if CurrentItemNo not in ItemNo:
-#         ItemNo.append(CurrentItemNo)
-#     else:
-#         print("Item number is not unique, please try again.")
-#         continue
-#         CurrentDescription = str(input("Please enter the item description: "))
-#         Description.append(CurrentDescription)
-#         CurrentReservePrice = float(input("Please enter the price: "))
-#         ReservePrice.append(CurrentReservePrice)
-#         NumberOfBid.append(0)
+while True:
+        product_selling = int(input("Number of selling items: "))
+        if product_selling < 10:
+            print("Please enter an item number bigger than 10.")
+            continue
+        else:
+            break
+
+item_name_list = []
+bid_number_list = [0]*product_selling
+description_list = []
+reserve_price_list = []
+item_number_list = list(range(1, product_selling+1))
+product_selling = 0
+
+for i in range(product_selling):
+    input_name = str(input("Please enter the item name: "))
+    item_name_list.append(input_name)
+    input_description = str(input("Please enter the item description: "))
+    description_list.append(input_description)
+    input_price = float(input("Please enter the reserve price: "))
+    reserve_price_list.append(input_price)
 print("\n")
-print(ItemNo)
-print(ItemName)
-print(Description)
-print(ReservePrice)
+print(item_number_list)
+print(item_name_list)
+print(description_list)
+print(reserve_price_list)
--- Task 3.py
+++ Task 3.py
@@ -3,11 +3,28 @@
 
 # Task 1
 
-item_name_list = ["phone", "postcard", "car", "pen", "tablet", "pencil case", "laptop", "mouse", "keyboard", "收藏加购物车十元返现"]
-bid_number_list = [0]*10
-description_list = ["iPhone XS", "New Zealand City View", "BMW i8", "Schneider Limited Edition", "iPad Pro 12.9in", "Kokuyo Limited", "ASUS Zenbook Pro 15", "Magic Mouse", "Cherry MX. 8.0", "勿拍此项"]
-reserve_price_list = [300, 15, 50000, 100, 2027, 50, 1099, 129, 699, 1000000000]
-item_number_list = list(range(1, 11))
+while True:
+        product_selling = int(input("Number of selling items: "))
+        if product_selling < 10:
+            print("Please enter an item number bigger than 10.")
+            continue
+        else:
+            break
+
+item_name_list = []
+bid_number_list = [0]*product_selling
+description_list = []
+reserve_price_list = []
+item_number_list = list(range(1, product_selling+1))
+product_selling = 0
+
+for i in range(product_selling):
+    input_name = str(input("Please enter the item name: "))
+    item_name_list.append(input_name)
+    input_description = str(input("Please enter the item description: "))
+    description_list.append(input_description)
+    input_price = float(input("Please enter the reserve price: "))
+    reserve_price_list.append(input_price)
 
 # Task 2
 
